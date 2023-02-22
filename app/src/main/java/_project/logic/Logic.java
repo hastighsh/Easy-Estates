2405ec2ad60239b31d07aac02013467f27995284
Jayant Varma <87495514+Jayant1Varma@users.noreply.github.com>
2023-02-21 20:28:38 -0500

    Update Logic.java

--- app/src/main/java/_project/logic/Logic.java
+++ app/src/main/java/_project/logic/Logic.java
@@ -13,6 +13,8 @@ public class Logic implements LogicIF{
 	
 	//must have attributes by default:
 	DataBaseIF db;
+	int visualizations_running;
+	
 	// another one is <<parameter>>, but since we get what we need from the constructor, we ommit having it here.
 
 	public static void main(String[] args) {
@@ -28,22 +30,30 @@ public class Logic implements LogicIF{
 		
 	}
 	
-	public void AddTimeSeries(ArrayList<Location> loc, Time startTime, Time endTime){
+	public void AddTimeSeries(Location place, Time startTime, Time endTime){	// use case 1
 		
 		
 		
 	}
 	
-	public void loadData(){
+	public void loadData(){		// not sure if this function should exist. Probably a UI function where the user simply passes the value 
 		
 		
 	}
 	
-	public DataTable dataFrame(boolean summaryTable) {	// called by UI using the toggle button
+	public DataTable fetchData(){	// fetch data from DB     	for use case 1, and multiple other uses
 		
 		DataTable data = new DataTable();
 		
-		// call to db to get raw data
+		return data;
+	
+	}
+	
+	public DataTable dataFrame(boolean summaryTable) {	// called by UI using the toggle button   // for use case 2
+		
+		DataTable data = new DataTable();
+		
+		// call to db to get raw data  fetchData() again
 		
 		return data;
 		
@@ -56,14 +66,20 @@ public class Logic implements LogicIF{
 		
 	}
 	
-	public DataTable configureVisualization(String VisualizationType) {
+	public DataTable configureVisualization(String VisualizationType) { // for use case 3
 		
 		DataTable configuredData = new DataTable(); 
 		
 		return configuredData;
 	}
 	
-	public DataTable visualize(DataTable configuredData) {
+	public DataTable visualize(DataTable configuredData) {	// for use case 3
+		
+		int threshold = 3;
+		
+		if(visualizations_running > threshold){
+			return null; //	RETURN SOME KIND OF AN EXCEPTION. MODIFY THIS NULL TO SOMETHING MEANINGFUL
+		}
 		
 		DataTable data = new DataTable();	// will hold the data required by UI to visualized
 		
@@ -71,7 +87,7 @@ public class Logic implements LogicIF{
 		
 	}
 	
-	public DataTable configureStats(String StatsType) {
+	public DataTable configureStats(String StatsType) {  // for use case 4
 		
 		DataTable configuredData = new DataTable(); 
 		
@@ -79,7 +95,7 @@ public class Logic implements LogicIF{
 	}
 	
 	
-	public StatsComparison CompareTimeSeries(TimeSeries ts1, TimeSeries ts2) { 
+	public StatsComparison compareTimeSeries(TimeSeries ts1, TimeSeries ts2) { // for use case 4
 		
 		boolean exists = checkExistence(ts1, ts2);
 		
@@ -87,12 +103,12 @@ public class Logic implements LogicIF{
 		return s;
 	}
 
-	private boolean checkExistence(TimeSeries ts1, TimeSeries ts2) {		//helper method for StatsComparison
+	private boolean checkExistence(TimeSeries ts1, TimeSeries ts2) {		//helper method for StatsComparison. Similar helper can be used for use case 5
 		
 		return false;		
 	}
 	
-	public DataTable forecast(int forecastMonths, TimeSeries ts, Location l) {
+	public DataTable forecast(int forecastMonths, TimeSeries ts) {	// for use case 5
 		
 		return new DataTable();
 	}
