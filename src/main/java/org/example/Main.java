b235ab3b458dd4bfecefebaa504ba8a168beec43
K1fight <92151239+K1fight@users.noreply.github.com>
2023-02-23 15:29:47 -0500

    add parameter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,15 @@ package org.example;
 import backend.MySql;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         MySql my = new MySql();
+    }
+    public void querySet( ){
+        for(int i = 0;i< list.size();i++){
+
+        }
     }
 }
---
+++ src/main/java/org/example/Parameter.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public interface Parameter {
+    public void setStartTime(String time);
+    public void setEndTime(String time);
+    public Time getTime(String time);
+    pubkic Location
+
+}
