88b3e65c01c4aa353e531774cf22017074419aa3
K1fight <92151239+K1fight@users.noreply.github.com>
2023-03-21 20:02:04 -0400

    Deliverable 2 UI ver. 5

--- app/src/main/java/UI/UI/MainUI.java
+++ app/src/main/java/UI/UI/MainUI.java
@@ -87,7 +87,7 @@ public class MainUI extends JFrame {
     JComboBox<String> fromList, toList;
     JPanel west;
     JFreeChart chart;
-    ChartPanel chartPanel;
+    ChartPanel chartPanel,chartTimeSerisePanel;
     JScrollPane outputScrollPane;
     Logic log = new Logic();
     String startTime= "";
@@ -169,10 +169,12 @@ public class MainUI extends JFrame {
                 if(!locations.contains((String)countriesList.getSelectedItem())){
                     locations.add((String)countriesList.getSelectedItem());
                     counter++;
+                    west.remove(chartTimeSerisePanel);
                     west.remove(chartPanel);
                     west.remove(outputScrollPane);
                     createReport(west);
                     createLine(west);
+                    createTimeSeries(west);
                     SwingUtilities.updateComponentTreeUI(west);
                 }
 
@@ -200,10 +202,12 @@ public class MainUI extends JFrame {
                     }
                     locations.remove((String)countriesList.getSelectedItem());
                     counter--;
+                    west.remove(chartTimeSerisePanel);
                     west.remove(chartPanel);
                     west.remove(outputScrollPane);
                     createReport(west);
                     createLine(west);
+                    createTimeSeries(west);
                     SwingUtilities.updateComponentTreeUI(west);
                 }
             }
@@ -255,10 +259,12 @@ public class MainUI extends JFrame {
                                     result.add(new Node(location.getName(),get));
                                 }
                             }
+                            west.remove(chartTimeSerisePanel);
                             west.remove(chartPanel);
                             west.remove(outputScrollPane);
                             createReport(west);
                             createLine(west);
+                            createTimeSeries(west);
                             SwingUtilities.updateComponentTreeUI(west);
                         } catch (SQLException ex) {
                             throw new RuntimeException(ex);
@@ -290,10 +296,7 @@ public class MainUI extends JFrame {
 
         Vector<String> viewsNames = new Vector<String>();
         viewsNames.add("Line Chart");
-//        viewsNames.add("Pie Chart");
-//        viewsNames.add("Bar Chart");
-//        viewsNames.add("Scatter Chart");
-//        viewsNames.add("Report");
+
         JComboBox<String> viewsList = new JComboBox<String>(viewsNames);
         JButton addView = new JButton("+");
         JButton removeView = new JButton("-");
@@ -360,10 +363,10 @@ public class MainUI extends JFrame {
     private void createCharts(JPanel west) {
         createReport(west);
         createLine(west);
-//        createTimeSeries(west);
-//        createBar(west);
-//        createPie(west);
-//        createScatter(west);
+        createTimeSeries(west);
+        createBar(west);
+        createPie(west);
+        createScatter(west);
 
     }
 
@@ -623,56 +626,56 @@ public class MainUI extends JFrame {
 
 
     private void createTimeSeries(JPanel west) {
-        TimeSeries series1 = new TimeSeries("Mortality/1000 births");
-        series1.add(new Year(2018), 5.6);
-        series1.add(new Year(2017), 5.7);
-        series1.add(new Year(2016), 5.8);
-        series1.add(new Year(2015), 5.8);
-        series1.add(new Year(2014), 5.9);
-        series1.add(new Year(2013), 6.0);
-        series1.add(new Year(2012), 6.1);
-        series1.add(new Year(2011), 6.2);
-        series1.add(new Year(2010), 6.4);
+//        TimeSeries series1 = new TimeSeries("Mortality/1000 births");
+//        series1.add(new Year(2018), 5.6);
+//        series1.add(new Year(2017), 5.7);
+//        series1.add(new Year(2016), 5.8);
+//        series1.add(new Year(2015), 5.8);
+//        series1.add(new Year(2014), 5.9);
+//        series1.add(new Year(2013), 6.0);
+//        series1.add(new Year(2012), 6.1);
+//        series1.add(new Year(2011), 6.2);
+//        series1.add(new Year(2010), 6.4);
 
-        TimeSeries series2 = new TimeSeries("Health Expenditure per Capita");
-        series2.add(new Year(2018), 10624);
-        series2.add(new Year(2017), 10209);
-        series2.add(new Year(2016), 9877);
-        series2.add(new Year(2015), 9491);
-        series2.add(new Year(2014), 9023);
-        series2.add(new Year(2013), 8599);
-        series2.add(new Year(2012), 8399);
-        series2.add(new Year(2011), 8130);
-        series2.add(new Year(2010), 7930);
-        TimeSeriesCollection dataset2 = new TimeSeriesCollection();
-        dataset2.addSeries(series2);
+//        TimeSeries series2 = new TimeSeries("Health Expenditure per Capita");
+//        series2.add(new Year(2018), 10624);
+//        series2.add(new Year(2017), 10209);
+//        series2.add(new Year(2016), 9877);
+//        series2.add(new Year(2015), 9491);
+//        series2.add(new Year(2014), 9023);
+//        series2.add(new Year(2013), 8599);
+//        series2.add(new Year(2012), 8399);
+//        series2.add(new Year(2011), 8130);
+//        series2.add(new Year(2010), 7930);
+//        TimeSeriesCollection dataset2 = new TimeSeriesCollection();
+//        dataset2.addSeries(series2);
 
-        TimeSeries series3 = new TimeSeries("Hospital Beds/1000 people");
-        series3.add(new Year(2018), 2.92);
-        series3.add(new Year(2017), 2.87);
-        series3.add(new Year(2016), 2.77);
-        series3.add(new Year(2015), 2.8);
-        series3.add(new Year(2014), 2.83);
-        series3.add(new Year(2013), 2.89);
-        series3.add(new Year(2012), 2.93);
-        series3.add(new Year(2011), 2.97);
-        series3.add(new Year(2010), 3.05);
+//        TimeSeries series3 = new TimeSeries("Hospital Beds/1000 people");
+//        series3.add(new Year(2018), 2.92);
+//        series3.add(new Year(2017), 2.87);
+//        series3.add(new Year(2016), 2.77);
+//        series3.add(new Year(2015), 2.8);
+//        series3.add(new Year(2014), 2.83);
+//        series3.add(new Year(2013), 2.89);
+//        series3.add(new Year(2012), 2.93);
+//        series3.add(new Year(2011), 2.97);
+//        series3.add(new Year(2010), 3.05);
 
         TimeSeriesCollection dataset = new TimeSeriesCollection();
-        dataset.addSeries(series1);
-        dataset.addSeries(series3);
+//        dataset.addSeries(series1);
+//        dataset.addSeries(series3);
 
         XYPlot plot = new XYPlot();
         XYSplineRenderer splinerenderer1 = new XYSplineRenderer();
         XYSplineRenderer splinerenderer2 = new XYSplineRenderer();
 
-        plot.setDataset(0, dataset);
+        plot.setDataset(0, dataset());
         plot.setRenderer(0, splinerenderer1);
         DateAxis domainAxis = new DateAxis("Year");
         plot.setDomainAxis(domainAxis);
         plot.setRangeAxis(new NumberAxis(""));
 
-        plot.setDataset(1, dataset2);
+//        plot.setDataset(1, dataset2);
         plot.setRenderer(1, splinerenderer2);
         plot.setRangeAxis(1, new NumberAxis("US$"));
 
@@ -682,7 +685,7 @@ public class MainUI extends JFrame {
         JFreeChart chart = new JFreeChart("Mortality vs Expenses & Hospital Beds",
                 new Font("Serif", java.awt.Font.BOLD, 18), plot, true);
 
-        ChartPanel chartPanel = new ChartPanel(chart);
+        chartTimeSerisePanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new Dimension(400, 300));
         chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
         chartPanel.setBackground(Color.white);
@@ -690,10 +693,11 @@ public class MainUI extends JFrame {
 
     }
 
-    private void addParameter(){}
     private ArrayList<Node> getInfo(){
         return result;
     }
+
+
     private XYSeriesCollection dataset(){
         double year = 0;
         double month = 0;
@@ -713,6 +717,9 @@ public class MainUI extends JFrame {
         }
         return dataset;
     }
+//    private DefaultCategoryDataset datasetD(){
+//
+//    }
     private ArrayList<Double> merge(Node node){
         ArrayList<Double> result = new ArrayList<>();
         double temp = 0;
