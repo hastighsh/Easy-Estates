7abc62335cc142ad938791840528786cbf6dcafa
K1fight <92151239+K1fight@users.noreply.github.com>
2023-03-21 20:27:46 -0400

    Deliverable 2 UI ver. 5.1

--- app/src/main/java/UI/UI/MainUI.java
+++ app/src/main/java/UI/UI/MainUI.java
@@ -174,7 +174,7 @@ public class MainUI extends JFrame {
                     west.remove(outputScrollPane);
                     createReport(west);
                     createLine(west);
-                    createTimeSeries(west);
+//                    createTimeSeries(west);
                     SwingUtilities.updateComponentTreeUI(west);
                 }
 
@@ -207,7 +207,7 @@ public class MainUI extends JFrame {
                     west.remove(outputScrollPane);
                     createReport(west);
                     createLine(west);
-                    createTimeSeries(west);
+//                    createTimeSeries(west);
                     SwingUtilities.updateComponentTreeUI(west);
                 }
             }
@@ -626,44 +626,44 @@ public class MainUI extends JFrame {
 
 
     private void createTimeSeries(JPanel west) {
-//        TimeSeries series1 = new TimeSeries("Mortality/1000 births");
-//        series1.add(new Year(2018), 5.6);
-//        series1.add(new Year(2017), 5.7);
-//        series1.add(new Year(2016), 5.8);
-//        series1.add(new Year(2015), 5.8);
-//        series1.add(new Year(2014), 5.9);
-//        series1.add(new Year(2013), 6.0);
-//        series1.add(new Year(2012), 6.1);
-//        series1.add(new Year(2011), 6.2);
-//        series1.add(new Year(2010), 6.4);
+        TimeSeries series1 = new TimeSeries("Mortality/1000 births");
+        series1.add(new Year(2018), 5.6);
+        series1.add(new Year(2017), 5.7);
+        series1.add(new Year(2016), 5.8);
+        series1.add(new Year(2015), 5.8);
+        series1.add(new Year(2014), 5.9);
+        series1.add(new Year(2013), 6.0);
+        series1.add(new Year(2012), 6.1);
+        series1.add(new Year(2011), 6.2);
+        series1.add(new Year(2010), 6.4);
 
-//        TimeSeries series2 = new TimeSeries("Health Expenditure per Capita");
-//        series2.add(new Year(2018), 10624);
-//        series2.add(new Year(2017), 10209);
-//        series2.add(new Year(2016), 9877);
-//        series2.add(new Year(2015), 9491);
-//        series2.add(new Year(2014), 9023);
-//        series2.add(new Year(2013), 8599);
-//        series2.add(new Year(2012), 8399);
-//        series2.add(new Year(2011), 8130);
-//        series2.add(new Year(2010), 7930);
-//        TimeSeriesCollection dataset2 = new TimeSeriesCollection();
-//        dataset2.addSeries(series2);
+        TimeSeries series2 = new TimeSeries("Health Expenditure per Capita");
+        series2.add(new Year(2018), 10624);
+        series2.add(new Year(2017), 10209);
+        series2.add(new Year(2016), 9877);
+        series2.add(new Year(2015), 9491);
+        series2.add(new Year(2014), 9023);
+        series2.add(new Year(2013), 8599);
+        series2.add(new Year(2012), 8399);
+        series2.add(new Year(2011), 8130);
+        series2.add(new Year(2010), 7930);
+        TimeSeriesCollection dataset2 = new TimeSeriesCollection();
+        dataset2.addSeries(series2);
 
-//        TimeSeries series3 = new TimeSeries("Hospital Beds/1000 people");
-//        series3.add(new Year(2018), 2.92);
-//        series3.add(new Year(2017), 2.87);
-//        series3.add(new Year(2016), 2.77);
-//        series3.add(new Year(2015), 2.8);
-//        series3.add(new Year(2014), 2.83);
-//        series3.add(new Year(2013), 2.89);
-//        series3.add(new Year(2012), 2.93);
-//        series3.add(new Year(2011), 2.97);
-//        series3.add(new Year(2010), 3.05);
+        TimeSeries series3 = new TimeSeries("Hospital Beds/1000 people");
+        series3.add(new Year(2018), 2.92);
+        series3.add(new Year(2017), 2.87);
+        series3.add(new Year(2016), 2.77);
+        series3.add(new Year(2015), 2.8);
+        series3.add(new Year(2014), 2.83);
+        series3.add(new Year(2013), 2.89);
+        series3.add(new Year(2012), 2.93);
+        series3.add(new Year(2011), 2.97);
+        series3.add(new Year(2010), 3.05);
 
         TimeSeriesCollection dataset = new TimeSeriesCollection();
-//        dataset.addSeries(series1);
-//        dataset.addSeries(series3);
+        dataset.addSeries(series1);
+        dataset.addSeries(series3);
 
         XYPlot plot = new XYPlot();
         XYSplineRenderer splinerenderer1 = new XYSplineRenderer();
@@ -675,7 +675,7 @@ public class MainUI extends JFrame {
         plot.setDomainAxis(domainAxis);
         plot.setRangeAxis(new NumberAxis(""));
 
-//        plot.setDataset(1, dataset2);
+        plot.setDataset(1, dataset2);
         plot.setRenderer(1, splinerenderer2);
         plot.setRangeAxis(1, new NumberAxis("US$"));
 
@@ -685,7 +685,7 @@ public class MainUI extends JFrame {
         JFreeChart chart = new JFreeChart("Mortality vs Expenses & Hospital Beds",
                 new Font("Serif", java.awt.Font.BOLD, 18), plot, true);
 
-        chartTimeSerisePanel = new ChartPanel(chart);
+        ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new Dimension(400, 300));
         chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
         chartPanel.setBackground(Color.white);
